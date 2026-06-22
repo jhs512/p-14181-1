49e611c7f1e69699ea8fd158e317ede1a3b4133c
jhs512 <jangka512@gmail.com>
2026-06-23 00:43:53 +0900

    130-3

--- src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
@@ -61,14 +61,11 @@ public class ApiV1AdmMemberControllerTest {
 
     @Test
     @DisplayName("다건조회, without permission")
+    @WithUserDetails("user1")
     void t3() throws Exception {
-        Member actor = memberService.findByUsername("user1").get();
-        String actorApiKey = actor.getApiKey();
-
         ResultActions resultActions = mvc
                 .perform(
                         get("/api/v1/adm/members")
-                                .header("Authorization", "Bearer " + actorApiKey)
                 )
                 .andDo(print());
 
@@ -81,16 +78,13 @@ public class ApiV1AdmMemberControllerTest {
 
     @Test
     @DisplayName("단건조회")
+    @WithUserDetails("admin")
     void t2() throws Exception {
-        Member actor = memberService.findByUsername("admin").get();
-        String actorApiKey = actor.getApiKey();
-
         int id = 1;
 
         ResultActions resultActions = mvc
                 .perform(
                         get("/api/v1/adm/members/" + id)
-                                .header("Authorization", "Bearer " + actorApiKey)
                 )
                 .andDo(print());
 
@@ -109,16 +103,13 @@ public class ApiV1AdmMemberControllerTest {
 
     @Test
     @DisplayName("단건조회, without permission")
+    @WithUserDetails("user1")
     void t4() throws Exception {
-        Member actor = memberService.findByUsername("user1").get();
-        String actorApiKey = actor.getApiKey();
-
         int id = 1;
 
         ResultActions resultActions = mvc
                 .perform(
                         get("/api/v1/adm/members/" + id)
-                                .header("Authorization", "Bearer " + actorApiKey)
                 )
                 .andDo(print());
 
