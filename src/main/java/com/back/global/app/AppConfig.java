deafb79b4e7b02b4cb89b7b29031b058d7623a55
jhs512 <jangka512@gmail.com>
2026-06-23 00:32:16 +0900

    127

--- src/main/java/com/back/global/app/AppConfig.java
+++ src/main/java/com/back/global/app/AppConfig.java
@@ -1,11 +1,13 @@
 package com.back.global.app;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class AppConfig {
@@ -35,5 +37,13 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Getter
+    private static ObjectMapper objectMapper;
+
+    @Autowired
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        AppConfig.objectMapper = objectMapper;
     }
 }
